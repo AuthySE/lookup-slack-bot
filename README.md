ce9598518feb081fe346e2c2bcc0c7a9b0bc2525
Josh <josh@twilio.com>
2018-03-23 14:44:51 -0700

    Reformatting some code.  Adding docs.

--- README.md
+++ README.md
@@ -1,44 +1,49 @@
 # lookup-slack-bot
 
-This is the slack bot that responds to the `/lookup` commands in Twilio's slack instance.
+This is the slack bot that responds to the `/lookup` commands in your Slack instance.
+You can try a publically available [Lookup](https://www.twilio.com/lookup) or when logged into your [Twilio Console](https://www.twilio.com/console/lookup)
 
-## architecture
+
+## Architecture
 
 ```
 +----------------------+                   +---------------------------+
 |                      |                   |                           |
-|                      +-----------------> |     Twilio Function       |
-|      Slack Bot/App   |                   |                           |
+|     Slack Bot/App    +-----------------> |     Twilio Function       |
+|                      |                   |                           |
 |   (runs in Slack]    |                   |        (function.js)      |
 |                      | <---------------+ |                           |
 +----------------------+                   +---------------------------+
 ```
 
-## Slack Bot Setup
-
-(Currently, Twilio internal only). 
-https://api.slack.com/apps/A64CF5C5P/general?
-
-### Setup
-
-1. Set Features -> Interactive Components -> Request URL to your Twilio function URL
-2. Create Features -> Slash Command (ex. `/lookup`). Set it's request URL to the Twilio function URL 
-
-### Ideas
-
-- [ ] Todo : Make the slack bot public and allow users to link it with their own hosted Twilio function or Twilio account. 
-
+## Twilio Add-Ons Setup
+1. Enable Add-ons in the [Console](twilio.com/console/lookup/add-ons)
+    *  Make sure you filter the list by the Lookup Category
+1. Take note of the unique name for each enabled add-on.
+1. Update the list of add-ons at the top of the function.js file.
 
 ## Twilio Function Setup
-
-Currently, runs in @devarshi 's Twilio account.
-
-### Setup
-
 1. Setup a function with the code in function.js
-2. Turn off signature validation
-3. Enable ACCOUNT_SID and AUTH_TOKEN here : https://www.twilio.com/console/runtime/functions/configure
-4. Add LOOKUP_SLACKBOT_TOKEN env variable here : https://www.twilio.com/console/runtime/functions/configure . Set it to the Verification Token from your Slackbot's Settings -> Basic Information -> App Credentials -> Verification Token
+1. Turn off signature validation
+1. Enable ACCOUNT_SID and AUTH_TOKEN here : https://www.twilio.com/console/runtime/functions/configure
+1. Add LOOKUP_SLACKBOT_TOKEN env variable here : https://www.twilio.com/console/runtime/functions/configure . 
+    * Set it to the Verification Token from your Slackbot's Settings -> Basic Information -> App Credentials -> Verification Token
 
-5. If you want to use it with the Add-ons, make sure those Add-ons are installed in your Twilio account.
+## Slack Bot Setup
+1. Browse to [api.slack.com](https://api.slack.com/) and login.
+1. Create a new Slack App.   Name the app and select your development Slack workspace.
+1. Open the newly created app.
+1. Browse to:  Basic Information -> Add Features and Functionality 
+    1. Interactive Components -> Add the Twilio Function URL as the `Request URL`
+    1. Create Features -> Slash Command (e.g. `/lookup`).
+1. Browse to Install App to install it in your workspace.
+
+## Using Lookup in Slack
+1. Wait a few minutes.  Perhaps restart Slack.
+1. Simply type `/lookup 12024561414`
+1. Select different enabled Lookup addons from the dropdown for additional information
+
+### Lookup in Slack
+![Lookup in Slack]()
+
 
--- function.js
+++ function.js
@@ -1,91 +1,104 @@
 // This is meant to be deployed as a Twilio function   
-  
-  exports.handler = function(context, event, callback) {
-	const client = context.getTwilioClient();
-	console.log(event);
-  
-  	var payload;
-  	if ("payload" in event) {
-      payload = JSON.parse(event.payload);
-      console.log(payload);
+
+exports.handler = function (context, event, callback) {
+
+    /**
+     * List of all add-ons available for Lookup as of 3/23/2018.
+     * Your Lookup Add-on value is a unique name you set during the installation in the Twilio Console.
+     * @type {[{}]}
+     */
+    let lookupOptions = [
+        {"text": "Data Axle BizInfo", "value": "dataaxle_bizinfo"},
+        {"text": "Whitepages Pro Caller Identification", "value": "whitepages_pro_caller_id"},
+        {"text": "Whitepages Pro Phone Intelligence", "value": "whitepages_pro_phone_intel"},
+        {"text": "Whitepages Pro Phone Reputation", "value": "whitepages_pro_phone_rep"},
+        {"text": "Advanced Caller ID by Next Caller", "value": "nextcaller_advanced_caller_id"},
+        {"text": "Twilio Carrier Information", "value": "twilio_carrier_info"},
+        {"text": "Twilio Caller Name", "value": "twilio_caller_name"},
+        {"text": "Payfone TCPA Compliance", "value": "payfone_tcpa_compliance"},
+        {"text": "Marchex Clean Call", "value": "marchex_cleancall"},
+        {"text": "Nomorobo Spam Score", "value": "nomorobo_spamscore"},
+        {"text": "IceHook Systems Scout", "value": "icehook_scout1"},
+        {"text": "Digital Segment Business Information", "value": "digitalsegment_businessinfo"},
+        {"text": "TrueSpam by TrueCNAM", "value": "truecnam_truespam"},
+        {"text": "OpenCNAM by Telo", "value": "telo_opencnam"},
+        {"text": "RealPhoneValidation RPV Turbo", "value": "real_phone_validation_rpv_turbo"}
+    ];
+
+    let addons = [];
+    let params = [];
+    let payload;
+
+    const client = context.getTwilioClient();
+    // console.info("Event information passed into the function ", event);
+
+    if ("payload" in event) {
+        payload = JSON.parse(event.payload);
+        // console.info("Payload information in the event object ", payload);
     } else {
-      payload = event;
+        payload = event;
     }
-  
-  	if (payload.token !== context.LOOKUP_SLACKBOT_TOKEN) {
-      callback(null, "Unauthorized access");
+
+    if (payload.token !== context.LOOKUP_SLACKBOT_TOKEN) {
+        callback(null, "Unauthorized access");
     }
-  
-  	// split event.text. expected format : "<e164 number> <add-on unique name, optional>"
-  	var params = [];
-  	var addons = [];
-  	
-  	if ("text" in payload) {
-      if (payload.text.trim().length > 0) {
-        params = payload.text.trim().split(" ");
-      }       
+
+    // split event.text. expected format : "<e164 number> <add-on unique name, optional>"
+    if ("text" in payload) {
+        if (payload.text.trim().length > 0) {
+            params = payload.text.trim().split(" ");
+        }
     } else if ("actions" in payload) {
         params = payload.actions[0].selected_options[0].value.trim().split(" ");
     }
-  
-  	console.log(params)
-  
-  	switch(params.length) {
-      case 0:
-	    callback(null, "Usage: /lookup <e164 number> <add-on unique name(s) comma separated>.");
-        break;
-      case 1:
-        addons.push('twilio_carrier_info');
-        break;        
-      case 2:
-        addons = params[1].split(",");                	    
-      break;
-    }
-  
-  	var slackResponse = {};
-  	//slackResponse.text = "Looking up " + params[0] + " using " + addons;
-  	slackResponse.response_type = "in_channel";
-  	slackResponse.attachments = [{},{}];
-    slackResponse.attachments[0].text =  "";
-    slackResponse.attachments[0].title = "Looking up " + params[0] + " using " + addons;
-  	slackResponse.attachments[0].title_link = "https://www.twilio.com/console/add-ons/product/lookup";
-	slackResponse.attachments[0].color =  "#36a64f";
-	slackResponse.attachments[0].mrkdwn_in = ["text","pretext"];
-  	//slackResponse.attachments[0].footer = "Lookup Slash command - still in development. Rough edges, etc. #team-marketplace for support.";
 
-   	slackResponse.attachments[0].callback_id = "addon_selection";
-  	slackResponse.attachments[0].actions = [{}];
-	slackResponse.attachments[0].actions[0].name = "addons_list";
-	slackResponse.attachments[0].actions[0].type = "select";
-  	slackResponse.attachments[0].actions[0].text = "Select another Add-on";
-  	slackResponse.attachments[0].actions[0].options = 
-      [ {"text":"Twilio Carrier Info","value":params[0] + " twilio_carrier_info"},
-        {"text":"RealPhoneValidation","value":params[0] + " real_phone_validation_rpv_turbo"},
-		{"text":"Whitepages Pro Phone Intel","value":params[0] + " whitepages_pro_phone_intel"},
-		{"text":"Icehook Scout","value":params[0] + " icehook_scout"},
-		{"text":"Twilio Caller Name","value":params[0] + " twilio_caller_name"},
-		{"text":"Whitepages Pro Caller ID","value":params[0] + " whitepages_pro_caller_id"},
-		{"text":"Nextcaller Advanced Caller ID","value":params[0] + " nextcaller_advanced_caller_id"},
-		{"text":"Telo OpenCNAM","value":params[0] + " telo_opencnam"},
-		{"text":"Digital Segment Business Intel","value":params[0] + " digitalsegment_businessinfo"},
-		{"text":"Whitepages Pro Phone Reputation","value":params[0] + " whitepages_pro_phone_rep"},             			    {"text":"Nomorobo Spam Score","value":params[0] + " nomorobo_spamscore"},  			              			
-        {"text":"TruSpam by TruCNAM","value":params[0] + " truecnam_truespam"}];
-  
-  	slackResponse.attachments[1].color =  "#36a64f";
-	slackResponse.attachments[1].mrkdwn_in = ["text","pretext"];
-  	slackResponse.attachments[1].footer = "Lookup Slash command - still in development. Rough edges, etc. #team-marketplace for support.";
-  
-  
-    client.lookups.v1.phoneNumbers(params[0]).fetch({'addOns':addons}).then(function(number){ 
-      // success
-      console.log(number);
-      slackResponse.attachments[1].text = "```" + JSON.stringify(number.addOns.results, null, 4) + " ```";
-      callback(null, slackResponse);
-    },
-                                                                            function(reason) {
-      // rejection
-      console.log(reason);
-      callback(null, "Lookup failed with error: " + reason.message);
+    switch (params.length) {
+        case 0:
+            callback(null, "Usage: /lookup <e164 number> <add-on unique name(s) comma separated>.");
+            break;
+        case 1:
+            addons.push('twilio_carrier_info');
+            break;
+        case 2:
+            addons = params[1].split(",");
+            break;
+    }
+
+    // adding the submitted phone number to the string with addons.
+    for (let i = 0; i < lookupOptions.length; i++) {
+        lookupOptions[i].value = `${params[0]} ${lookupOptions[i].value}`
+    }
+
+    // console.info("Lookup Options: ", lookupOptions);
+
+    let slackResponse = {};
+    slackResponse.response_type = "in_channel";
+    slackResponse.attachments = [{}, {}];
+    slackResponse.attachments[0].text = "";
+    slackResponse.attachments[0].title = "Looking up " + params[0] + " using " + addons;
+    slackResponse.attachments[0].title_link = "https://www.twilio.com/console/add-ons/product/lookup";
+    slackResponse.attachments[0].color = "#36a64f";
+    slackResponse.attachments[0].mrkdwn_in = ["text", "pretext"];
+
+    slackResponse.attachments[0].callback_id = "addon_selection";
+    slackResponse.attachments[0].actions = [{}];
+    slackResponse.attachments[0].actions[0].name = "addons_list";
+    slackResponse.attachments[0].actions[0].type = "select";
+    slackResponse.attachments[0].actions[0].text = "Select another Add-on";
+    slackResponse.attachments[0].actions[0].options = lookupOptions;
+
+    slackResponse.attachments[1].color = "#36a64f";
+    slackResponse.attachments[1].mrkdwn_in = ["text", "pretext"];
+    slackResponse.attachments[1].footer = "Lookup Slash command - Instructions and code here: github.com/AuthySE/lookup-slack-bot";
+
+    client.lookups.v1.phoneNumbers(params[0]).fetch({'addOns': addons}).then(function (number) {
+        // success
+        console.error(number);
+        slackResponse.attachments[1].text = "```" + JSON.stringify(number.addOns.results, null, 4) + " ```";
+        callback(null, slackResponse);
+    }, function (reason) {
+        // rejection
+        callback(null, "Lookup failed with error: " + reason.message);
     });
-  
+
 };
